7e76835106bf32aa085db584ee28b5b08f62751c
mrvzrkgl <20201101063@ogr.msgsu.edu.tr>
2023-02-25 01:03:41 +0300

    r script added

--- supermarket_sales.R
+++ supermarket_sales.R
@@ -1,135 +1,110 @@
----
-title: "**Supermarket Sales (Süpermarket Satışları)**"
-author: "**Merve Zirekoğlu**"
-date: "2023-01-31"
-output:
-  html_document:
-    css: styles.css
-    toc: true
-    toc_float: true
-    toc_depth: 5
-    number_sections: true
-    smooth_scroll: false
-  rmdformats::material:
-    highlight: tango  
----
+
+### Supermarket Sales (Süpermarket Satışları)
 
 
-# **Giriş**
+## Giriş
+
+# Supermarket sales veri seti, bir süpermarketin 3 farklı şubesinin satış 
+# bilgilerini içermektedir.
+# Veri seti değişkenleri tanıtılarak sırasıyla kategorik değişken analizleri,
+# satış analizleri ve müşteri davranışı analizleri yapılacaktır. 
+
+# Veri seti 1000 gözlemden ve 17 değişkenden oluşmaktadır. Ayrıca kayıp değer içermemektedir.
 
 
-![](C:/Users/zrkgl/Desktop/project/supermarket.png)
+# KAYNAK: <https://www.kaggle.com/datasets/aungpyaeap/supermarket-sales> 
+  
 
-
-
-Supermarket sales veri seti, bir süpermarketin 3 farklı şubesinin satış bilgilerini içermektedir.
-Veri seti değişkenleri tanıtılarak sırasıyla kategorik değişken analizleri,satış analizleri ve müşteri davranışı analizleri yapılacaktır. 
-
-Veri seti 1000 gözlemden ve 17 değişkenden oluşmaktadır. Ayrıca kayıp değer içermemektedir.
-
-
-KAYNAK: <https://www.kaggle.com/datasets/aungpyaeap/supermarket-sales> 
-
-
-
-# **Veri seti değişkenleri**
-
-1. Invoice id: Fatura ID
-2. Branch: Süpermarket şubesi (A, B, C)
-3. City: Süpermarketin bulunduğu şehir
-4. Customer type: Müşteri türü
-    * Member = Üye kartı olanlar
-    * Normal = Üye kartı olmayanlar
-5. Gender: Cinsiyet
-    * Female = Kadın 
-    * Male = Erkek
-6. Product line: Ürün grupları 
-    * Electronic accessories = Elektronik aksesuarlar 
-    * Fashion accessories = Moda aksesuarları
-    * Food and beverages = Yiyecek ve içecekler
-    * Health and beauty = Sağlık ve güzellik
-    * Home and lifestyle = Ev ve yaşam tarzı 
-    * Sports and travel = Spor ve seyahat
-7. Unit price: Ürünlerin $ cinsinden birim fiyatı
-8. Quantity: Müşteri tarafından satın alınan ürün sayısı
-9. Tax: Müşteri alımları için %5 vergi ücreti
-10. Total: Vergi dahil toplam fiyat
-11. Date: Satın alma tarihi (Ocak 2019 - Mart 2019)
-12. Time: Satın alma zamanı (10.00 - 21.00)
-13. Payment: Ödeme yöntemi 
-    * Cash = Nakit
-    * Credit card = Kredi kartı
-    * Ewallet = E-cüzdan
-14. COGS: Satılan malların maliyeti
-15. Gross margin percentage: Brüt kar marjı yüzdesi
-16. Gross income: Brüt gelir
-17. Rating: Alışveriş deneyimine ilişkin müşteri derecelendirmesi (1 - 10 arası)
+  
+## Veri seti değişkenleri
+  
+# 1. Invoice id: Fatura ID
+# 2. Branch: Süpermarket şubesi (A, B, C)
+# 3. City: Süpermarketin bulunduğu şehir
+# 4. Customer type: Müşteri türü
+#   * Member = Üye kartı olanlar
+#   * Normal = Üye kartı olmayanlar
+# 5. Gender: Cinsiyet
+#   * Female = Kadın 
+#   * Male = Erkek
+# 6. Product line: Ürün grupları 
+#   * Electronic accessories = Elektronik aksesuarlar 
+#   * Fashion accessories = Moda aksesuarları
+#   * Food and beverages = Yiyecek ve içecekler
+#   * Health and beauty = Sağlık ve güzellik
+#   * Home and lifestyle = Ev ve yaşam tarzı 
+#   * Sports and travel = Spor ve seyahat
+# 7. Unit price: Ürünlerin $ cinsinden birim fiyatı
+# 8. Quantity: Müşteri tarafından satın alınan ürün sayısı
+# 9. Tax: Müşteri alımları için %5 vergi ücreti
+# 10. Total: Vergi dahil toplam fiyat
+# 11. Date: Satın alma tarihi (Ocak 2019 - Mart 2019)
+# 12. Time: Satın alma zamanı (10.00 - 21.00)
+# 13. Payment: Ödeme yöntemi 
+#   * Cash = Nakit
+#   * Credit card = Kredi kartı
+#   * Ewallet = E-cüzdan
+# 14. COGS: Satılan malların maliyeti
+# 15. Gross margin percentage: Brüt kar marjı yüzdesi
+# 16. Gross income: Brüt gelir
+# 17. Rating: Alışveriş deneyimine ilişkin müşteri derecelendirmesi (1 - 10 arası)
 
 
 
-Verimizi import edelim.
+# Verimizi import edelim.
 
-```{r}
 library(readxl)
 getwd()
 setwd("C:/Users/zrkgl/Desktop/project")
 sales <- read_xlsx("supermarket_sales.xlsx")
 View(sales)
-```
 
 
 
-Time değişkeni için zaman formatını düzenleyelim.
+# Time değişkeni için zaman formatını düzenleyelim.
 
-```{r}
 library(tidyverse)
 sales <- sales %>%
   mutate(
     Time = format(Time, "%H:%M:%S"))
 View(sales)
-```
 
 
 
-Kayıp gözlem kontrolü yapalım.
+# Kayıp gözlem kontrolü yapalım.
 
-```{r}
 table(is.na(sales))
-```
 
 
+# Kaç gözlem olduğuna bakalım.
 
-Kaç gözlem olduğuna bakalım.
-
-```{r}
 nrow(sales)
-```
-
-Toplamda 1000 kişinin kaydı tutulmuştur.
 
 
-
-
-# **Kategorik Değişken Analizleri**
-
-Branch , city, customer type, gender, product line, payment değişkenleri; kategorik değişkenlerdir.
-Kategorik değişkenleri inceleyelim.
+# Toplamda 1000 kişinin kaydı tutulmuştur.
 
 
 
-## **Şubelere Talep Sayısı**
 
-Branch; 
+## Kategorik Değişken Analizleri
 
-```{r}
+# Branch , city, customer type, gender, product line, payment değişkenleri;
+# kategorik değişkenlerdir.
+# Kategorik değişkenleri inceleyelim.
+
+
+
+## Şubelere Talep Sayısı
+
+# Branch; 
+
 table(sales$Branch)
-```
-
-A şubesinden 340 , B şubesinden 332 ve C şubesinden 328 kişi alışveriş yapmıştır. 
-Şubelerden alışveriş yapan kişi sayıları birbirine oldukça yakındır. 
 
 
-```{r}
+# A şubesinden 340 , B şubesinden 332 ve C şubesinden 328 kişi alışveriş yapmıştır. 
+# Şubelerden alışveriş yapan kişi sayıları birbirine oldukça yakındır. 
+
+
 ggplot(sales, aes(x = Branch)) +
   geom_bar(aes(fill = Branch), stat = "count") +
   ggtitle("Şubelere Talep Sayısı") +
@@ -137,25 +112,22 @@ ggplot(sales, aes(x = Branch)) +
   ylab("Şubelere Talep Sayısı") +
   scale_fill_manual(values = c("khaki" , "rosybrown", "lightsalmon"), name = "Şubelere Talep Sayısı") +
   theme(plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-    axis.title.x = element_text(size = 12, face = "bold"),
-    axis.title.y = element_text(size = 12, face = "bold"),
-    axis.text.x = element_text(size = 10 , face = "bold"),
-    axis.text.y = element_text(size = 10 , face = "bold"))
-```
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 10 , face = "bold"),
+        axis.text.y = element_text(size = 10 , face = "bold"))
 
 
-## **Şehirlere Talep Sayısı**
+## Şehirlere Talep Sayısı
 
-City;
+# City;
 
-```{r}
 table(sales$City)
-```
 
-A şubesi Yangon'da, B şubesi Mandalay'da ve C şubesi ise Naypyitaw'da bulunmaktadır.
+# A şubesi Yangon'da, B şubesi Mandalay'da ve C şubesi ise Naypyitaw'da bulunmaktadır.
 
 
-```{r}
+
 ggplot(sales, aes(x = City)) +
   geom_bar(aes(fill = City), stat = "count") +
   ggtitle("Şehirlere Talep Sayısı") +  
@@ -168,22 +140,18 @@ ggplot(sales, aes(x = City)) +
         axis.text.x = element_text(size = 10 , face = "bold"),
         axis.text.y = element_text(size = 10 , face = "bold"))
 
-```
 
 
 
-## **Müşteri Türü**
+## Müşteri Türü
 
-Customer type;
+# Customer type;
 
-```{r}
 table(sales$`Customer type`)
-```
 
-Alışveriş yapanların 501'i üye olan, 499'u ise üye olmayan müşteridir.
+# Alışveriş yapanların 501'i üye olan, 499'u ise üye olmayan müşteridir.
 
 
-```{r}
 ggplot(sales, aes(x = `Customer type`)) +
   geom_bar(aes(fill = `Customer type`), stat = "count") +
   ggtitle("Müşteri Türü Dağılımı") +
@@ -195,21 +163,18 @@ ggplot(sales, aes(x = `Customer type`)) +
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(size = 10 , face = "bold"),
         axis.text.y = element_text(size = 10 , face = "bold"))
-```
 
 
-## **Cinsiyet Dağılımı**
+## Cinsiyet Dağılımı
 
-Gender;
+# Gender;
 
-```{r}
 table(sales$Gender)
-```
-
-Alışveriş yapanların 501'i kadın ve 499'u erkektir.
 
 
-```{r}
+# Alışveriş yapanların 501'i kadın ve 499'u erkektir.
+
+
 ggplot(sales, aes(x = Gender)) +
   geom_bar(aes(fill = Gender), stat = "count") +
   ggtitle("Cinsiyet Dağılımı") +
@@ -221,21 +186,20 @@ ggplot(sales, aes(x = Gender)) +
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text( size = 10 , face = "bold"),
         axis.text.y = element_text( size = 10 , face = "bold"))
-```
 
 
-## **Ürün Kategorileri**
+## Ürün Kategorileri
 
-Product line;
+# Product line;
 
-```{r}
 table(sales$`Product line`)
-```
-
-Elektronik aksesuarlar için 170, Moda aksesuarları için 178 , Yiyecek ve içecekler için 174, Sağlık ve güzellik için 152, Ev ve yaşam tarzı için 160, Spor ve seyahat için 166 satış gerçekleşmiştir.
 
 
-```{r}
+# Elektronik aksesuarlar için 170, Moda aksesuarları için 178 , 
+# Yiyecek ve içecekler için 174, Sağlık ve güzellik için 152, 
+# Ev ve yaşam tarzı için 160, Spor ve seyahat için 166 satış gerçekleşmiştir.
+
+
 sales %>%
   ggplot(aes(x=`Product line`)) +
   geom_bar(fill = c("powderblue" , "rosybrown2" , "palegreen" , "mediumorchid2" , "lightgoldenrod" , "lightsalmon")) +
@@ -248,20 +212,17 @@ sales %>%
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(angle = 45 , size = 10 , face = "bold"),
         axis.text.y = element_text(angle = 0 , size = 10 , face = "bold"))
-```
 
 
-## **Ödeme Yöntemleri**
+## Ödeme Yöntemleri
 
-Payment;
+# Payment;
 
-```{r}
 table(sales$Payment)
-```
 
-344 nakit ödeme, 311 kredi kartı ödemesi ve 345 e-cüzdan ödemesi alınmıştır.
 
-```{r}
+# 344 nakit ödeme, 311 kredi kartı ödemesi ve 345 e-cüzdan ödemesi alınmıştır.
+
 ggplot(sales, aes(x = Payment)) +
   geom_bar(stat = "count", fill = c("khaki" , "rosybrown", "lightsalmon")) +
   ggtitle("Ödeme Yöntemi Dağılımı") +
@@ -273,19 +234,17 @@ ggplot(sales, aes(x = Payment)) +
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(size = 10 , face = "bold"),
         axis.text.y = element_text(size = 10 , face = "bold"))
-```
 
 
 
-# **Satış Analizleri**
+## Satış Analizleri
 
 
-## **Ürün Satış Analizi**
+## Ürün Satış Analizi
 
 Şubelere göre en çok satılan ürün kategorilerini belirleyelim
 
 
-```{r}
 # Verileri Branch ve Product line değişkenlerine göre gruplandıralım
 grouped_data <- sales %>%
   group_by(Branch, `Product line`) %>%
@@ -298,17 +257,15 @@ top_product_lines <- grouped_data %>%
   top_n(1, quantity)
 
 top_product_lines
-```
 
 
-A şubesi için 371 ürün ile Home and lifestyle grubu
-B	şubesi için 322 ürün ile Sports and travel grubu
-C	şubesi için 369 ürün ile Food and beverages	grubu en çok satılan kategoridir.
+# A şubesi için 371 ürün ile Home and lifestyle grubu
+# B	şubesi için 322 ürün ile Sports and travel grubu
+# C	şubesi için 369 ürün ile Food and beverages	grubu en çok satılan kategoridir.
 
 
-Her şube için en çok satılan ürün kategorilerinin satışlarını görselleştirelim.
+# Her şube için en çok satılan ürün kategorilerinin satışlarını görselleştirelim.
 
-```{r}
 top_product_lines$Branch <- factor(top_product_lines$Branch, 
                                   levels = c("A", "B", "C"),
                                   labels = c("Home and lifestyle", "Sports and travel", "Food and beverages"))
@@ -323,22 +280,19 @@ ggplot(top_product_lines, aes(x = reorder(Branch, -quantity), y = quantity, fill
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(angle = 45, hjust = 1, size = 10, face = "bold"),
         axis.text.y = element_text(angle = 0, hjust = 1, size = 10, face = "bold"))
-```
 
 
 
-## **Satış Miktarı Analizi**
+## Satış Miktarı Analizi
 
-Her bir şubedeki ürün satış miktarlarını karşılaştıralım.
+# Her bir şubedeki ürün satış miktarlarını karşılaştıralım.
 
 
-```{r}
 # Her şube için toplam satın alınan ürün sayısını hesaplayalım
 branch_sales <- aggregate(Quantity ~ Branch, data = sales, sum)
 branch_sales
-```
 
-```{r}
+
 # Yeniden sıralayalım
 branch_sales <- branch_sales[order(-branch_sales$Quantity), ]
 
@@ -355,32 +309,26 @@ ggplot(branch_sales, aes(x = 1, y = Quantity, fill = Branch)) +
   scale_fill_manual(values = c("powderblue" , "rosybrown2" ,"lightsalmon")) +
   theme_void() +
   geom_text(aes(label = paste0(Branch, "\n", Percentage, "%")), position = position_stack(vjust = 0.5))
-```
 
 
 
 
-## **Şubelere Göre Zamana Bağlı Satış Analizi**
+## Şubelere Göre Zamana Bağlı Satış Analizi
 
-```{r}
 # Date değişkeninin tarih formatında olduğundan emin olalım
 class(sales$Date)
 
 library(lubridate)
 sales$Date <- ymd(sales$Date)
 class(sales$Date)
-```
 
 
-```{r}
 # Şubelerin satışlarını aylara göre gruplandıralım
 sales %>%
   group_by(month = format(Date, "%Y-%m"), Branch) %>%
   summarise(mean = mean(Total))
-```
 
 
-```{r}
 # Görselleştirme yapalım
 ggplot(data = sales, aes(x = as.Date(Date, format = "%Y-%m-%d"), y = Total, fill = Branch)) +
   geom_area(position = "stack") +
@@ -393,19 +341,17 @@ ggplot(data = sales, aes(x = as.Date(Date, format = "%Y-%m-%d"), y = Total, fill
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(size = 10, face = "bold"),
         axis.text.y = element_text(size = 10, face = "bold"))
-```
 
 
-# **Müşteri Davranış Analizleri**
+## Müşteri Davranış Analizleri
 
-## **Müşteri Davranışları ve Satış Analizi**
+## Müşteri Davranışları ve Satış Analizi
 
-Müşterilerin satın alma alışkanlıklarını ve satışlar üzerindeki etkilerini inceleyelim.
+# Müşterilerin satın alma alışkanlıklarını ve satışlar üzerindeki etkilerini inceleyelim.
 
 
-Müşteri başına satış grafiği;
+# Müşteri başına satış grafiği;
 
-```{r}
 sales %>%
   group_by(`Invoice ID`, Date) %>%
   summarise(total_sale = sum(Total),
@@ -420,14 +366,12 @@ sales %>%
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(size = 10, face = "bold"),
         axis.text.y = element_text(size = 10, face = "bold"))
-```
 
 
 
-Satın alınan ürün grafiği;
+# Satın alınan ürün grafiği;
 
 
-```{r}
 sales %>%
   group_by('Invoice ID', `Product line`) %>%
   summarise(total_sale = sum(Total),
@@ -443,16 +387,14 @@ sales %>%
         axis.title.y = element_text(size = 12, face = "bold"),
         axis.text.x = element_text(angle = 50 , size = 10 , face = "bold"),
         axis.text.y = element_text(angle = 0 , size = 10 , face = "bold"))
-```
 
 
 
-## **Güne Bağlı Satış Analizi**
+## Güne Bağlı Satış Analizi
 
 
-Hangi günlerde daha fazla satış yapıldığını inceleyelim.
+# Hangi günlerde daha fazla satış yapıldığını inceleyelim.
 
-```{r}
 # Tarih sütununu tarih formatına dönüştürelim
 sales$Date <- as.Date(sales$Date, "%m/%d/%Y")
 
@@ -473,22 +415,21 @@ ggplot(sales_by_day, aes(x = Day, y = total_sales, group = 1)) +
         axis.title.y = element_text(size = 14, face = "bold"),
         axis.text.x = element_text(angle = 0 , size = 10 , face = "bold"),
         axis.text.y = element_text(angle = 0 , size = 10 , face = "bold"))
-```
 
 
-Grafikten hareketle en çok satışın Cumartesi, en az satışın ise Pazartesi günlerinin gerçekleştiğini söyleyebiliriz.
-
-
-
-
-## **Saate Bağlı Satış Analizi**
-
-
-Hangi saatlerde daha fazla satış yapıldığını inceleyelim.
+# Grafikten hareketle en çok satışın Cumartesi, en az satışın ise Pazartesi
+# günlerinin gerçekleştiğini söyleyebiliriz.
 
 
 
-```{r}
+
+## Saate Bağlı Satış Analizi
+
+
+# Hangi saatlerde daha fazla satış yapıldığını inceleyelim.
+
+
+
 # Satın alma saatlerini "hour" adında yeni bir sütuna ekleyelim
 sales$hour <- format(as.POSIXct(sales$Time, format = "%H:%M:%S"), format = "%H")
 
@@ -506,12 +447,10 @@ ggplot(sales_by_hour, aes(x = hour, y = total_sales, group = 1)) +
         axis.title.y = element_text(size = 14, face = "bold"),
         axis.text.x = element_text(angle = 0 , size = 10 , face = "bold"),
         axis.text.y = element_text(angle = 0 , size = 10 , face = "bold"))
-```
 
 
 
-
-Grafikten hareketle en çok satışın saat 19:00'da, en az satışın ise saat 20:00'da yapıldığını söyleyebiliriz.
+# Grafikten hareketle en çok satışın saat 19:00'da, en az satışın ise saat 20:00'da yapıldığını söyleyebiliriz.
 
 
 
